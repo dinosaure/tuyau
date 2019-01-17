bb6908b683099cc4d7672c4e285b30b7de87a0f4
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-01-17 21:52:26 +0100

    Add documentation

--- lib/resolver.mli
+++ lib/resolver.mli
@@ -1,23 +1,46 @@
 module Make (IO : Sigs.IO) : sig
   type t
+  (** A map of resolvers. *)
 
   module Identifier : sig
     type t = private int
+    (** Uniq identifier of a resolver. *)
 
     val compare : t -> t -> int
+    (** comparison function of {!identifier}. *)
+
     val equal : t -> t -> bool
+    (** equal function of {!identifier}. *)
   end
 
   type 'v resolver
+  (** A resolver key. *)
 
   val make : name:string -> 'v resolver
+  (** Create a new resolver key. *)
+
   val name : 'v resolver -> string
+  (** [name key] returns name of resolver [key]. *)
+
   val identifier : 'v resolver -> Identifier.t
+  (** [identifier key] returns uniq identifier of resolver [key]. *)
 
   val empty : t
+  (** An empty map of key resolvers. *)
+
   val add : 'v resolver -> resolve:(Identifier.t list -> Domain_name.t -> 'v option IO.t) -> t -> t
+  (** [add key impl t] returns a map containing the binding of [key] to [impl].
+     If [key] was already bound in [t] to a value that is physically equal to
+     [impl], [t] is returned unchanged. *)
+
   val rem : 'v resolver -> t -> t
+  (** [remove key t] returns a map containing the same bindings as [t], expect
+     for [key] which is unbound in the returned map. *)
+
   val get : 'v resolver -> t -> (Identifier.t list -> Domain_name.t -> 'v option IO.t)
+  (** [get key t] returns the current binding of [key] in [t]. *)
 
   val resolve : ?colored:Identifier.t list -> Domain_name.t -> 'v resolver -> t -> 'v option IO.t
+  (** [resolve ~colored domain key t] tries to resolve [domain] with the binded
+     resolver [key] with an already visited (colored) list of resolvers. *)
 end
--- lib/service.mli
+++ lib/service.mli
@@ -2,36 +2,58 @@ module Make (IO : Sigs.IO) (B : Sigs.SINGLETON) : sig
   module Resolver : module type of Resolver.Make(IO)
 
   type kind = UDP | TCP
+  (** Kind of ground protocol. *)
 
   type desc =
     { name : string
     ; port : int
     ; kind : kind }
+  (** Description of a service. *)
 
   module type SERVICE = Sigs.SERVICE
     with type description = desc
      and type 'a io = 'a IO.t
      and type buffer = B.t
+  (** Specialized signature which a service implementation must respect. *)
+
   module type FLOW = Sigs.FLOW
     with type 'a io = 'a IO.t
      and type buffer = B.t
+  (** Specialized signature of a flow. Subset of {!SERVICE}. *)
 
   type ('e, 'f) service =
     { desc : desc
     ; implementation : (module SERVICE with type endpoint = 'e and type flow = 'f) }
+  (** Packed value of a service implementation binded with a description. *)
 
   val of_module : name:string -> port:int -> kind:kind ->
     (module SERVICE with type endpoint = 'e
                      and type flow = 'f)
     -> ('e, 'f) service
+  (** [of_module ~name ~port ~kind (module Service)] makes a {!service} from an
+     implementation and a description. *)
 
   type 'f scheme
+  (** A witness of a scheme. *)
 
   type flow
+  (** Abstract type of a flow implementation. *)
 
   val register : 'e Resolver.resolver -> ('e, 'f) service -> 'f scheme
+  (** [register key service] returns a witness which is an association between
+     the resolver [key] and the service [service]. The association is only on
+     the type-system. Then, binded implementation of [key] will be done the
+     resolution and give the result to the service implementation. *)
+
   val flow : 'f scheme -> 'f -> flow
+  (** [flow witness flow] hides a physical value of a flow represented by
+     [witness] to abstract type {!flow}. *)
+
   val resolve : Domain_name.t -> Resolver.t -> 'f scheme -> flow -> (flow, [ `Unresolved | `Msg of string ]) result IO.t
+  (** [resolve domain resolvers witness flow] tries to resolve [domain] with
+     binded resolver to [witness] (see {!register}) available in [resolvers].
+     Then, iff binded resolver find a solution, {!SERVICE.init} is called with
+     unwrapped/unhidden [flow] with solution. *)
 
   val extract : 'f scheme -> flow -> ('f -> (module FLOW with type flow = 'f) -> 'a) -> ('a, [ `Msg of string ]) result
   val bind : 'f scheme -> flow -> ('f -> flow) -> flow option
