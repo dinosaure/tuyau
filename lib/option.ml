1b7dcf536427980baf160b99fb25ac26f836fc9c
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-01-20 11:58:52 +0100

    Delete old version of tuyau

--- lib/option.ml
+++
@@ -1,9 +0,0 @@
-type 'a t = 'a option
-
-let map f = function
-  | Some x -> Some (f x)
-  | None -> None
-
-let value ~default = function
-  | Some x -> x
-  | None -> default
--- lib/tuyau.ml
+++
@@ -1,6 +0,0 @@
-module Resolver = Resolver
-module Service = Service
-module Like_conduit = Like_conduit
-module High = High
-module Sigs = Sigs
-module Scheme = Scheme
